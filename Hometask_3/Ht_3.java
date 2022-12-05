47a3541e2656518608d81e12ba5a01c19548b423
AndrewDar <geologalone@gmail.com>
2022-12-05 16:13:58 +0300

    program complited

--- Hometask_3/Ht_3.java
+++ Hometask_3/Ht_3.java
@@ -1,56 +1,65 @@
 package Hometask_3;
 
+import java.io.IOException;
+
 public class Ht_3 {
-
-    public static long[] passFromAToB(int a, int b, int c, int d) {
-        if (a > b || a == b) {
-            return null;
-        } else {
-            
-            long[] step = new long[b-a+1];
-           step[0]=1;
-            for (int i = 1, x=a+1; i <= b-a; i++, x++) {
-                
-                
-                //if (a * c != i && a + d != i)
-                //{step[i] = 0;}
-                    
-                 if (x-d>=a) {
-
-                    step[i] = step [i - d];
-                    
-
-                } if (x % c == 0 && x/c>=a) {
-                    step[i] += step[x / c-a];
-                    
-
-                }
-                
-                //System.out.printf("%s ", step[i]);
-
+    public static long passFromAToB(int a, int b, int c, int d) throws Exception {
+        if (a >= b)
+            return 0;
+        else {
+            long[] steps = new long[b - a + 1];
+            steps[0] = 1;
+            for (int i = 1, x = a + 1; i <= b - a; i++, x++) {
+                if (x - d >= a)
+                    steps[i] = steps[i - d];
+                if (x % c == 0 && x / c >= a)
+                    steps[i] += steps[x / c - a];
             }
-            return step;
+            return steps[steps.length - 1];
         }
     }
 
-    /*a: 2 b: 7 c: 2 d: 1 -> 3
-a: 3 b: 27 c: 3 d: 2 -> 6
-a: 30 b: 345 c: 5 d: 6 -> 0
-a: 30 b: 345 c: 2 d: 1 -> 7047
-a: 22 b: 333 c: 3 d: 1 -> 467
-a: 55 b: 555 c: 5 d: 2 -> 30
-a: 22 b: 2022 c: 11 d: 56 -> 0
-a: 22 b: 2022 c: 11 d: 10 -> 18
-a: 22 b: 2022 c: 3 d: 1 -> 763827
-a: 22 b: 20220 c: 3 d: 1 -> 535173226980
-a: 1 b: 1111 c: 2 d: 1 -> 3990330794
-a: 1 b: 11111 c: 2 d: 1 -> 606408167570737286*/
-
-    public static void main(String[] args) {
-        long[] res = passFromAToB(1, 11111, 2, 1);
-        
-        System.out.println(res[res.length-1]);
-
+    public static String showPath(int a, int b, int c, int d) {
+        int x = b;
+        String path = "";
+        while (x > a) {
+            if (x / c >= a && x % c == 0) {
+                path += "1";
+                x = x / c;
+            } else {
+                path += "2";
+                x = x - d;
+            }
+        }
+        path = reverseString(path);
+        path += b;
+        return path;
     }
 
+    public static String reverseString(String str) {
+        String revStr = "";
+        for (int i = str.length() - 1; i >= 0; i--) {
+            if (str.charAt(i) == '1')
+                revStr += "k1->";
+            else
+                revStr += "k2->";
+        }
+        return revStr;
+    }
+
+    public static void printResult(long num, String commands) {
+        if (num == 0)
+            System.out.println("No paths");
+        else {
+            System.out.printf("Total number of conversion paths - %s", num);
+            System.out.println();
+            System.out.printf("The best route is %s", commands);
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        long res = passFromAToB(1, 11111, 2, 1);
+        String com = showPath(1, 11111, 2, 1);
+        printResult(res, com);
+    }
 }
